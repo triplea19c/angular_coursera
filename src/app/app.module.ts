6f524742660befdb9e2092e863458b2e92986211
triplea19c <triplea19c@gmail.com>
2018-06-28 14:22:07 +0000

    Components Part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import { MatToolbarModule } from '@angular/material/toolbar'; 
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import 'hammerjs';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
+    MatListModule,
     BrowserAnimationsModule,
     MatToolbarModule,
     FlexLayoutModule
