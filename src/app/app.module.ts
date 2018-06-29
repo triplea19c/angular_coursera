b6ad35f0c88f725efadeb6ac154e2a4cceeb95f4
triplea19c <triplea19c@gmail.com>
2018-06-29 15:55:26 +0000

    angular components Part 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatListModule } from '@angular/material/list';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import 'hammerjs';
 import { AppComponent } from './app.component';
@@ -15,7 +17,9 @@ import { MenuComponent } from './menu/menu.component';
   ],
   imports: [
     BrowserModule,
-    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule,
     BrowserAnimationsModule,
     MatToolbarModule,
     FlexLayoutModule
